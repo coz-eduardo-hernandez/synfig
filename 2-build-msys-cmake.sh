9cec26ea59f2279f93a7f5148cf24ba484fe8039
coz <coz.eduardo.hernandez@gmail.com>
2022-12-15 19:33:04 -0400

    Add color output to files generated by CMake

--- 2-build-msys-cmake.sh
+++ 2-build-msys-cmake.sh
@@ -2,6 +2,6 @@
 
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/opt/mlt-6.16.0/lib/pkgconfig"
 mkdir -p cmake-build-msys && cd cmake-build-msys
-cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=Release ..
+cmake -GNinja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=Release -DCMAKE_COLOR_DIAGNOSTICS=ON ..
 ninja
 ninja install > /dev/null
